6be74b2daac0bbe272e203d4c56168f885d9f1c8
Bedant sharma <manurishi1103@gmail.com>
2026-05-05 01:05:27 +0530

    feat: add CORS middleware for localhost:5173

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from supabase._async.client import create_client
 
 from clients import supabase_client
@@ -32,6 +33,14 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc",
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router=student_router)
